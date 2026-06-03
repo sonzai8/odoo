eb742f6290d24b1c5fe99b9afbd8185808465aae
sonzai <phanvanson.info@gmail.com>
2026-06-03 14:10:55 +0700

    sửa css hiển thị tên công ty, sửa lỗi export file công do xoá nhầm . thêm chức năng upload qrcode cccd

--- my_custom_addons/dl_salary_kpi/__manifest__.py
+++ my_custom_addons/dl_salary_kpi/__manifest__.py
@@ -31,8 +31,11 @@
     ],
     'assets': {
         'web.assets_backend': [
+            'dl_salary_kpi/static/src/css/web_overrides.css',
             'dl_salary_kpi/static/src/css/matrix.css',
             'dl_salary_kpi/static/src/css/full_width_matrix.css',
+            'dl_salary_kpi/static/src/libs/jsQR.js',
+            'dl_salary_kpi/static/src/js/qr_employee_field.js',
         ],
     },
     'installable': True,
--- my_custom_addons/dl_salary_kpi/controllers/__init__.py
+++ my_custom_addons/dl_salary_kpi/controllers/__init__.py
@@ -1,1 +1,3 @@
 from . import main
+from . import attendance_export
+from . import employee_export
